9438293a111be5f68cd7a45abedb4d3446815253
mtoriello0725 <34101012+mtoriello0725@users.noreply.github.com>
2018-11-19 12:47:33 -0500

    Adds the dynamic url name for each individual blog post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.views.generic import ListView, DetailView
 from blog.models import Post
 
 urlpatterns = [
-	path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")),name="blog/blog.html")
+	path('', ListView.as_view(queryset=Post.objects.all().order_by("-date"),template_name="blog/blog.html")),
+	re_path('(?P<pk>(\d+))', DetailView.as_view(model=Post, template_name='blog/post.html'))
 ]
