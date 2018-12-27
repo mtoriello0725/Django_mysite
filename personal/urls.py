70a804dfbfee53ca09bd8a44a85cd4d30620c74a
mtoriello0725 <34101012+mtoriello0725@users.noreply.github.com>
2018-12-27 14:39:57 -0500

    Changed homepage, and migrated to most recent version of bootstrap

--- personal/urls.py
+++ personal/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('contact/', views.contact, name='contact')
+	path('about/', views.about, name='about')
 ]
--- personal/views.py
+++ personal/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'personal/home.html')
 
-def contact(request):
-	return render(request, 'personal/basic.html',
+def about(request):
+	return render(request, 'personal/about.html',
 		{'content': ['If you would like to contact me, please email','mtoriello0725@gmail.com']})
