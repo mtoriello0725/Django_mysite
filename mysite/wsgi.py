c6c9a1f12599f898c10215a81b1e1d3dd491a1b4
mtoriello0725 <34101012+mtoriello0725@users.noreply.github.com>
2019-01-05 15:50:18 -0500

    Added environment variable file and code to load file

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+project_folder = os.path.expanduser("~/mysite")
+load_dotenv(os.path.join(project_folder, ".env"))
